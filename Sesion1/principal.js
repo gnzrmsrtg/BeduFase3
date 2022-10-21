b207db1c54bf32f639fec2a79d2ce3810488c067
Gonzalo Ramos <gnzramos@gmail.com>
2022-10-20 19:43:45 -0500

    Completamos ejercicios de la sesion 1

--- Sesion1/principal.js
+++ Sesion1/principal.js
@@ -14,9 +14,12 @@ function divideFn(num1, num2) {
     console.log(num1 / num2)
 }
 
+random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
+
 module.exports = {
     sumarFn,
     restarFn,
     multiplicaFn,
-    divideFn
+    divideFn,
+    random
 }
